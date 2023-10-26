ef2c6a962eb9701f3e6abe6a9fac0a01db15adbe
John <78293728+Tostibrand@users.noreply.github.com>
2023-10-26 22:54:00 +0200

    Add resize based on grid size to function

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const container = document.querySelector(`.container`);
 
+// spwan a grid of a size and assign a unique id to each block in the grid
 function spawnGridSize(size){
     for(i = 1; i <= size; i++) {
     const div = document.createElement(`div`);
     div.setAttribute(`id`, i);
-    div.textContent = i
-    container.appendChild(div)
+    const blockSize = 100/size
+    div.style.width = String(blockSize + `%`)
+    div.style.height = String(blockSize + `%`)
+    // div.textContent = i
+    container.append(div)
     }
 
 }
