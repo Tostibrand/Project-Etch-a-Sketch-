4aff41a66907d128c7c959aa245e31188289fd89
Tostibrand <78293728+Tostibrand@users.noreply.github.com>
2023-10-27 07:47:55 +0200

    Update comments to correct syntax app.js

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ function spawnGridSize(size){
 //set unique id for each spawned block
     div.setAttribute(`id`, i);
     
-//Dynamically scale blocksize to always           fit the parent div no matter the amount of 
-blocks
+//Dynamically scale blocksize to always fit the parent div no matter the amount of blocks
     const blockSize = 100/Math.sqrt(size);
     div.style.width = String(blockSize + `%`);
     div.style.height = String(blockSize + `%`);
