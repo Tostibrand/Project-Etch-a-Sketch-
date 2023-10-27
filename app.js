bfce187d8f2104941759acd62400fd891aa80600
John <78293728+Tostibrand@users.noreply.github.com>
2023-10-27 22:59:11 +0200

    Add guard clause to reset gridsize to 0 x 0

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(`.container`);
 const input = document.querySelector(`#input`);
+const gridSize = document.querySelector(`#gridSize`);
 
 // event listeners
 input.addEventListener(`click`, () => {spawnGridSize()}) 
@@ -21,6 +22,9 @@ function spawnGridSize(){
     if (size > 10000){
         return alert(`Input exceeds the maximum allowed grid size of 10000 boxes, please input a number below the maximum`);
     }
+    if (size == null){
+        gridSize.textContent = `grid size: 0 x 0`
+    }
     // to round off the input to the closest square root number
     size = Math.floor(Math.sqrt(size));
     console.log(size)
@@ -35,6 +39,8 @@ function spawnGridSize(){
     div.style.width = String(blockSize + `%`);
     div.style.height = String(blockSize + `%`);
     
+    gridSize.textContent = `grid size: ${size} x ${size}`
+    gridSize.style.width = `fit-content`
     container.append(div);
     }
 
