8986abcd6a0df576c6106080a5e362e636fc2436
John <78293728+Tostibrand@users.noreply.github.com>
2023-10-30 23:31:20 +0100

    Add resetGrid function based on grid size

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const container = document.querySelector(`.container`);
 const input = document.querySelector(`#input`);
 const gridSize = document.querySelector(`#gridSize`);
+const reset = document.querySelector(`#reset`);
+const active = document.querySelectorAll(`.active`)
 
 // event listeners
 input.addEventListener(`click`, () => {spawnGridSize()})
+reset.addEventListener(`click`, () => {resetGrid()})
 container.addEventListener
 ('mousemove', (event) => {
     
@@ -17,6 +20,8 @@ container.addEventListener
 
 
 let defaultGrid = generateGridSize(16);
+
+let size = 16
 
 
 // spawn default grid size on first load and reload
@@ -46,34 +51,18 @@ function generateGridSize(size){
         }
 }
 
-// spwan a grid of a size and assign a unique id to each block in the grid
+// spwan a grid of a size
 function spawnGridSize(){
     // To clear the grid on function call
     container.innerHTML = ``
     // to prompt for user input for gridsize
-    let size = prompt(`Please enter grid size. Maximum grid size is 100 x 100`)
-
-    
+    size = prompt(`Please enter grid size. Maximum grid size is 100 x 100`)
     generateGridSize(size);
-
-
-//     // to round off the input to the closest square root number
-//     size = Math.floor(Math.sqrt(size));
-//     console.log(size)
-//     // size = size*size;
-//     for(i = 1; i <= (size*size); i++) {
-//     const div = document.createElement(`div`);
-// //set unique id for each spawned block
-//     div.setAttribute(`id`, i);
     
-// //Dynamically scale blocksize to always fit the parent div no matter the amount of blocks
-//     const blockSize = 100/size;
-//     div.style.width = String(blockSize + `%`);
-//     div.style.height = String(blockSize + `%`);
-    
-//     gridSize.textContent = `grid size: ${size} x ${size}`
-//     gridSize.style.width = `fit-content`
-//     container.append(div);
-//     }
 
+}
+
+function resetGrid(){
+    container.innerHTML = ``;
+    generateGridSize(size);
 }
